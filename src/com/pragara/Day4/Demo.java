a7eaa8634cebc9adaa999444221f455d76b71a71
Sukhmpreet <sukhm1103@gmail.com>
2022-11-07 20:47:20 -0500

    sukhm is a good girl

--- src/com/pragara/Day4/Demo.java
+++ src/com/pragara/Day4/Demo.java
@@ -6,11 +6,14 @@ public class itDemo {
         if(x>10 && x<20)
         {
             System.out.println("allowed to take ride");
+            boolean sukhm;
+            System.out.println(" sukhm is a good girl");
         }
         int month =3;
         if(month == 1 || month == 3 || month == 5){
             System.out.println("Month with 31 days");}
     }
+
     }
 
 
